74fd2202eac25b1c69e8b2e6cdfe0f2becabcbd3
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-27 14:55:17 +0100

    Not responsive done

--- src/Components/PricingOptins/PricingOptions.jsx
+++ src/Components/PricingOptins/PricingOptions.jsx
@@ -5,7 +5,7 @@ const PricingOptions = ({pricingOptionsPromise}) => {
 const pricingData = use(pricingOptionsPromise);
 console.log(pricingData);
     return (
-        <div className=' grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+        <div className='grid grid-cols-3  sm:grid-cols-1  gap-3 mx-2'>
             {
                 pricingData.map((pricingPlan)=><PricingOptionCard key={pricingData.id} pricingPlan={pricingPlan}></PricingOptionCard>)
             }
