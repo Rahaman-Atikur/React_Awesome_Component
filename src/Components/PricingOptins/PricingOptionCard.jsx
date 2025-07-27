e0ac7ac2e21f8e51522f16798851413102dec7d0
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-27 09:10:11 +0100

    not done respossive:

--- src/Components/PricingOptins/PricingOptionCard.jsx
+++ src/Components/PricingOptins/PricingOptionCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const PricingOptionCard = ({pricingPlan}) => {
+const PricingOptionCard = ({ pricingPlan }) => {
     console.log(pricingPlan);
     return (
-        <div>
+        <div className='w-2/5 rounded-4xl gap-2  bg-sky-200 px-5 '>
+
             <h2 className='text-7xl font-extrabold'>{pricingPlan.name}</h2>
             <h4 className='text-5xl font-bold'>{pricingPlan.price_per_month}</h4>
             <p className='text-2xl'>{pricingPlan.description}</p>
-            {pricingPlan.features.map((feature)=><p>{feature}</p>)}
+            {pricingPlan.features.map((feature) => <p>{feature}</p>)}
+
+
         </div>
     );
 };
--- src/Components/PricingOptins/PricingOptions.jsx
+++ src/Components/PricingOptins/PricingOptions.jsx
@@ -5,7 +5,7 @@ const PricingOptions = ({pricingOptionsPromise}) => {
 const pricingData = use(pricingOptionsPromise);
 console.log(pricingData);
     return (
-        <div>
+        <div className=' grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
             {
                 pricingData.map((pricingPlan)=><PricingOptionCard key={pricingData.id} pricingPlan={pricingPlan}></PricingOptionCard>)
             }
