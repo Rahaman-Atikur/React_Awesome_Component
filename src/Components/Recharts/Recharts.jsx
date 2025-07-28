231ae5dc1724848b9011e58c14f6a4be36ac4931
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-28 11:00:55 +0100

    Chart done

--- src/Components/Recharts/Recharts.jsx
+++ src/Components/Recharts/Recharts.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import { LineChart } from 'recharts';
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+const studentData = [
+    {
+        "student_name": "John Doe",
+        "physics_marks": 85,
+        "chemistry_marks": 90,
+        "math_marks": 78
+    },
+    {
+        "student_name": "Alice Smith",
+        "physics_marks": 92,
+        "chemistry_marks": 88,
+        "math_marks": 95
+    },
+    {
+        "student_name": "Ravi Kumar",
+        "physics_marks": 76,
+        "chemistry_marks": 81,
+        "math_marks": 69
+    }
+]
 const Recharts = () => {
     return (
         <div>
-            <LineChart width={500} height={200} ></LineChart>
-            <LineChart width={600} height={300} data={data}>
-                <Line dataKey="uv" />
-            </LineChart>
+            <BarChart width={730} height={250} data={studentData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="student_name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="physics_marks" fill="#8884d8" />
+                <Bar dataKey="math_marks" fill="#82ca9d" />
+            </BarChart>
         </div>
     );
 };
-
 export default Recharts;
